582c01a335b61360cbc47baba9a2779a27fdcc8a
Wander Hillen <wjw.hillen@gmail.com>
2018-04-22 18:41:36 +0200

    Add Throttled exception

--- src/prorate.cr
+++ src/prorate.cr
@@ -3,7 +3,18 @@ require "redis"
 require "digest"
 
 # TODO: Write documentation for `Prorate`
-module Prorate
+module Prorate  
+  class Throttled < Exception
+    getter retry_in_seconds : Int32
+    def initialize(try_again_in)
+      @retry_in_seconds = try_again_in
+      super("Throttled, please lower your temper and try again in #{retry_in_seconds} seconds")
+    end
+    def message : String
+      "Throttled, please lower your temper and try again in #{retry_in_seconds} seconds"
+    end
+  end
+
   class ScriptHashMismatch < Exception
   end
 
@@ -20,12 +31,13 @@ module Prorate
   CURRENT_SCRIPT_HASH = get_script_hash
 
   class Throttle
-    def initialize(throttle)
-      @name = name
+    getter discriminators
+    
+    def initialize(@name : String)
       @discriminators = [] of String
     end
 
-    def <<(discriminator)
+    def <<(discriminator : String)
       @discriminators << discriminator
     end
 
