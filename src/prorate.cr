a1664840df5415edb0d708fc356a2733ab5d2620
Wander Hillen <wjw.hillen@gmail.com>
2018-04-22 18:07:25 +0200

    Start porting over code from rb version

--- src/prorate.cr
+++ src/prorate.cr
@@ -1,6 +1,39 @@
 require "./prorate/*"
+require "redis"
+require "digest"
 
 # TODO: Write documentation for `Prorate`
 module Prorate
-  # TODO: Put your code here
+  class ScriptHashMismatch < Exception
+  end
+
+  class MisconfiguredThrottle < Exception
+  end
+
+
+  def self.get_script_hash
+    script_filepath = File.join(__DIR__,"prorate","rate_limit.lua")
+    script = File.read(script_filepath)
+    Digest::SHA1.hexdigest(script)
+  end
+
+  CURRENT_SCRIPT_HASH = get_script_hash
+
+  class Throttle
+    def initialize(throttle)
+      @name = name
+      @discriminators = [] of String
+    end
+
+    def <<(discriminator)
+      @discriminators << discriminator
+    end
+
+    def throttle!()
+      digest = Digest::SHA1.hexdigest(@discriminators.join(""))
+      identifier = [name, digest].join(":")
+      redis.evalsha(CURRENT_SCRIPT_HASH, [] of String, [identifier, bucket_capacity, leak_rate, block_for])
+    end
+
+  end
 end
