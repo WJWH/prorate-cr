3775f1ce300054cfae9e19dd140e8df70a477168
Wander Hillen <wjw.hillen@gmail.com>
2018-04-22 18:06:52 +0200

    Add spec for CURRENT_SCRIPT_HASH

--- spec/prorate_spec.cr
+++ spec/prorate_spec.cr
@@ -1,9 +1,19 @@
 require "./spec_helper"
 
 describe Prorate do
-  # TODO: Write tests
+  # TODO: Write more tests
 
   it "works" do
-    false.should eq(true)
+    true.should eq(true)
   end
+  
+  context "CURRENT_SCRIPT_HASH" do
+    it "should be the SHA1 hash of the LUA script" do
+      script_path = File.join(__DIR__, "../src/prorate/rate_limit.lua")
+      script = File.read(script_path)
+      Prorate::CURRENT_SCRIPT_HASH.should eq(Digest::SHA1.hexdigest(script))
+    end
+  end
+  
 end
+
