548ddc646ffce923b46904d2f537f707ff08e41e
Wander Hillen <wjw.hillen@gmail.com>
2018-07-07 19:25:09 +0200

    Fix type errors and typos

--- src/prorate.cr
+++ src/prorate.cr
@@ -5,8 +5,8 @@ require "digest"
 # TODO: Write documentation for `Prorate`
 module Prorate  
   class Throttled < Exception
-    getter retry_in_seconds : Int32
-    def initialize(try_again_in)
+    getter retry_in_seconds : Int64
+    def initialize(try_again_in : Int64)
       @retry_in_seconds = try_again_in
       super("Throttled, please lower your temper and try again in #{retry_in_seconds} seconds")
     end
@@ -47,19 +47,20 @@ module Prorate
 
     def throttle!
       digest = Digest::SHA1.hexdigest(@discriminators.join(""))
-      identifier = [name, digest].join(":")
+      identifier = [@name, digest].join(":")
       remaining_block_time, bucket_level = @redis.evalsha(CURRENT_SCRIPT_HASH, [] of String, [identifier, @bucket_capacity, @leak_rate, @block_for])
-      raise Throttled.new(remaining_block_time) if remaining_block_time > 0
+      raise Throttled.new(remaining_block_time.as(Int64)) if remaining_block_time.as(Int64) > 0
+      return nil
     rescue ex : Redis::Error
-      if e.message.include? "NOSCRIPT"
+      if ex.message.as(String).includes? "NOSCRIPT"
         # The Redis server has never seen this script before. Needs to run only once in the entire lifetime of the Redis server (unless the script changes)
         script_filepath = File.join(__DIR__,"prorate","rate_limit.lua")
         script = File.read(script_filepath)
-        raise ScriptHashMismatch if Digest::SHA1.hexdigest(script) != CURRENT_SCRIPT_HASH
+        raise ScriptHashMismatch.new if Digest::SHA1.hexdigest(script) != CURRENT_SCRIPT_HASH
         @redis.script_load(script)
         @redis.evalsha(CURRENT_SCRIPT_HASH, [] of String, [identifier, @bucket_capacity, @leak_rate, @block_for])
       else
-        raise e
+        raise ex
       end
     end
 
